99288a04be9b0ade0e2350fbed429a3c30f704c4
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-12-15 20:09:28 -0600

    refactor

--- seed-user.js
+++ seed-user.js
@@ -1,28 +1,28 @@
+import 'dotenv/config'
 import { faker } from '@faker-js/faker';
 import fs from 'fs';
 
-const LIMIT = 100_000;
-const BASE_USERS_QUERY = 'INSERT INTO users (username, email, password, created_at) VALUES ';
+const LIMIT = 10;
+const BASE_USERS_QUERY = 'INSERT INTO users (username, email, password, created_at, avatar_id) VALUES ';
 const BASE_ROLES_QUERY = 'INSERT INTO roles (role_name) VALUES ';
 const BASE_USER_ROLES_QUERY = 'INSERT INTO user_roles (user_id, role_id) VALUES ';
+const BASE_FILES_PUBLIC_QUERY = 'INSERT INTO files_public (url, key, public_name) VALUES ';
 
-const BASE_PATH = process.env.BASE_PATH || './src/database/seed';
-const FILE_NAME = process.env.FILE_NAME || 'seed.sql';
+const BASE_PATH = process.env.BASE_PATH
+const FILE_NAME = process.env.FILE_NAME
 
-
-const createRolesSql = () => {
-  const roles = ['ROLE_USER', 'ROLE_PREMIUM', 'ROLE_ADMIN'];
-
-  for (let i = 0; i < roles.length; i++) {
-    let sql;
-    if (i === roles.length - 1) sql = `('${roles[i]}');\n`;
-    else sql = `('${roles[i]}'),`;
-
-    fs.appendFileSync(`${BASE_PATH}/${FILE_NAME}`, sql);
-  }
+if (!BASE_PATH || !FILE_NAME) {
+  throw new Error('Missing env variables! (BASE_PATH, FILE_NAME)');
 }
 
-const createUsersSql = (id) => {
+const ROLES = ['ROLE_USER', 'ROLE_USER_PREMIUM', 'ROLE_ADMIN'];
+
+const generateRolesSql = (i) => {
+  const query = `('${ROLES[i]}')`
+  return i === ROLES.length - 1 ? `${query};\n` : `${query},`;
+}
+
+const generateUsersSql = (id) => {
   const user = {
     username: faker.internet.userName(),
     email: faker.internet.email(),
@@ -31,48 +31,57 @@ const createUsersSql = (id) => {
   };
   const date = new Date(user.createdAt).toISOString().slice(0, 19).replace('T', ' ');
 
-  let sql;
-  if (id === LIMIT) sql = `('${user.username}${id}', '${id}${user.email}', '${user.password}', '${date}');\n`;
-  else sql = `('${user.username}${id}', '${id}${user.email}', '${user.password}', '${date}'),`;
-
-  fs.appendFileSync(`${BASE_PATH}/${FILE_NAME}`, sql);
-
+  const query = `('${user.username}${id}', '${id}${user.email}', '${user.password}', '${date}', ${id})`
+  return id === LIMIT ? `${query};\n` : `${query},`;
 }
 
-
-const createRoles = () => {
-  fs.writeFileSync(`${BASE_PATH}/${FILE_NAME}`, BASE_ROLES_QUERY);
-  createRolesSql();
+const generateFilesPublicSql = (id) => {
+  const file = {
+    filename: faker.image.avatar(),
+    key: faker.datatype.uuid(),
+    publicName: faker.random.word(),
+  };
+  const query = `('${file.filename}', '${file.key}', '${file.publicName}')`
+  return id === LIMIT ? `${query};\n` : `${query},`;
 }
 
-const createUsers = () => {
-  fs.appendFileSync(`${BASE_PATH}/${FILE_NAME}`, BASE_USERS_QUERY);
-  for (let id = 1; id <= LIMIT; id++) {
-    createUsersSql(id);
+const generateUserRolesSql = (id) => {
+  const roleNumber = faker.datatype.number({ min: 1, max: 3 });
+  const userRoles = [];
+
+  for (let i = 1; i <= roleNumber; i++) {
+    let query = `(${id}, ${i})`;
+    userRoles.push(id === LIMIT && i === roleNumber ? `${query};\n` : `${query},`);
   }
+  return userRoles.join('');
 }
 
-const createUserRoles = () => {
-  fs.appendFileSync(`${BASE_PATH}/${FILE_NAME}`, BASE_USER_ROLES_QUERY);
-  for (let id = 1; id <= LIMIT; id++) {
-    const roleNumber = faker.datatype.number({ min: 1, max: 3 });
-    const isLast = id === LIMIT;
+const createQueries = () => {
+  
+  const rolesQuery = [];
+  const usersSqlQuery = [];
+  const filesPublicSqlQuery = [];
+  const userRolesQuery = [];
 
-    for (let i = 1; i <= roleNumber; i++) {
-      const sql = `(${id}, ${i}),`;
-      if (isLast && i === roleNumber) return fs.appendFileSync(`${BASE_PATH}/${FILE_NAME}`, sql.slice(0, -1) + ';\n');
-      fs.appendFileSync(`${BASE_PATH}/${FILE_NAME}`, sql);
-    }
+  for (let i = 0; i < ROLES.length; i++) {
+    rolesQuery.push(generateRolesSql(i));
   }
+  for (let id = 1; id <= LIMIT; id++) {
+    usersSqlQuery.push(generateUsersSql(id));
+    filesPublicSqlQuery.push(generateFilesPublicSql(id));
+    userRolesQuery.push(generateUserRolesSql(id));
+  }
+  fs.writeFileSync(`${BASE_PATH}/${FILE_NAME}`, `${BASE_ROLES_QUERY}${rolesQuery.join('')}`);
+  fs.appendFileSync(`${BASE_PATH}/${FILE_NAME}`, `${BASE_FILES_PUBLIC_QUERY}${filesPublicSqlQuery.join('')}`);
+  fs.appendFileSync(`${BASE_PATH}/${FILE_NAME}`, `${BASE_USERS_QUERY}${usersSqlQuery.join('')}`);
+  fs.appendFileSync(`${BASE_PATH}/${FILE_NAME}`, `${BASE_USER_ROLES_QUERY}${userRolesQuery.join('')}`);
 }
 
 const main = () => {
   const fileExists = fs.existsSync(`${BASE_PATH}/${FILE_NAME}`);
   if (fileExists) fs.unlinkSync(`${BASE_PATH}/${FILE_NAME}`);
 
-  createRoles();
-  createUsers();
-  createUserRoles()
+  createQueries()
 }
 
 main();
